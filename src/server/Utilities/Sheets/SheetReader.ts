9fb2998dc0b9566cff71d50605b0b5c3e86dda54
Eric Moran <emoran0403@gmail.com>
2022-08-11 18:01:34 -0400

    installed google apis, wrote read from sheet function

---
+++ src/server/Utilities/Sheets/SheetReader.ts
@@ -0,0 +1,35 @@
+import readline from "readline";
+import { google } from "googleapis";
+import { GOOGLEINFO } from "../../config";
+
+const sheets = google.sheets({ version: "v4", auth: GOOGLEINFO.googleApiKey });
+
+export const readSKUsFromSheet = async () => {
+  return new Promise<string[][]>((resolve, reject) => {
+    sheets.spreadsheets.values.get(
+      {
+        spreadsheetId: GOOGLEINFO.spreadSheetID,
+        range: "SKUHERE!A5:A10",
+      },
+      (err, res) => {
+        if (err) {
+          console.log("The API returned an error: " + err);
+          reject(err);
+        }
+
+        const rows = res!.data.values;
+
+        if (rows!.length) {
+          // console.log(`rows below:`);
+          // console.log(rows);
+          resolve(rows!);
+        } else {
+          console.log("No data found.");
+          reject("No data found.");
+        }
+      }
+    );
+  });
+};
+
+// console.log({ GOOGLEINFO });
