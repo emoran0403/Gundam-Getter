900a5e44ed281c13b148b657e483153e68b32a65
Eric Moran <emoran0403@gmail.com>
2022-08-17 10:36:41 -0400

    main page layout looks ok now

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -15,16 +15,21 @@ const App = (props: AppProps) => {
   return (
     <main className="container my-5">
       <h1 className="text-primary text-center">Welcome to Gundam Getter!</h1>
-      <h3 className="text-primary text-center">Hit that button to get start the process</h3>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          startProcess();
-        }}
-      >
-        Gundam Get!
-      </button>
-      {pressed && <div>OK! working on it, check the sheet in a few minutes</div>}
+      <h3 className="text-primary text-center">Hit that button start the process</h3>
+      <div className="d-flex justify-content-center">
+        {pressed ? (
+          <div>OK! working on it, check the sheet in a few minutes</div>
+        ) : (
+          <button
+            className="btn btn-primary my-2"
+            onClick={() => {
+              startProcess();
+            }}
+          >
+            Gundam Get!
+          </button>
+        )}
+      </div>
     </main>
   );
 };
