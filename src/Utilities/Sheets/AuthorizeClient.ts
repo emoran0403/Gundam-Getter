7881db52b163604ee187b439a878ecbcd1cc1a66
Eric Moran <emoran0403@gmail.com>
2022-08-18 17:03:31 -0400

    google auth more attempt

--- src/Utilities/Sheets/AuthorizeClient.ts
+++ src/Utilities/Sheets/AuthorizeClient.ts
@@ -14,15 +14,21 @@ export function authorizeAndWrite(data) {
   let wow = { client_secret: CLIENTINFO.clientSecret, client_id: CLIENTINFO.clientID, redirect_uris: ["http://localhost:3000"] };
   const { client_secret, client_id, redirect_uris } = wow;
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+  console.log(`client without creds`);
+  console.log(oAuth2Client);
+  oAuth2Client.setCredentials(JSON.parse(process.env.GOOGLE_TOKEN));
+  console.log(`client with creds`);
+  console.log(oAuth2Client);
+  console.log(`now calling write function`);
+  writeValuesToSheet(oAuth2Client, data);
 
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    console.log(oAuth2Client);
-    if (err) return getNewToken(oAuth2Client, () => writeValuesToSheet(oAuth2Client, data));
-    oAuth2Client.setCredentials(JSON.parse(token));
-    console.log(oAuth2Client);
-    writeValuesToSheet(oAuth2Client, data);
-  });
+  // fs.readFile(TOKEN_PATH, (err, token) => {
+  //   // console.log(`first client below`);
+  //   // console.log(oAuth2Client);
+  //   if (err) return getNewToken(oAuth2Client, () => writeValuesToSheet(oAuth2Client, data));
+  //   // console.log(oAuth2Client);
+  // });
 }
 
 /**
@@ -31,41 +37,41 @@ export function authorizeAndWrite(data) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getNewToken(oAuth2Client, callback) {
-  // If modifying these scopes, delete token.json.
-  // const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
-  // // The file token.json stores the user's access and refresh tokens, and is
-  // // created automatically when the authorization flow completes for the first
-  // // time.
-  // const readline = require("readline");
-  // const authUrl = oAuth2Client.generateAuthUrl({
-  //   access_type: "offline",
-  //   scope: SCOPES,
-  // });
-  // if (process.env.GOOGLE_TOKEN) {
-  oAuth2Client.setCredentials(process.env.GOOGLE_TOKEN);
-  fs.writeFile(TOKEN_PATH, JSON.stringify(process.env.GOOGLE_TOKEN), (err) => {
-    if (err) return console.error(err);
-    console.log("Token stored to", TOKEN_PATH);
-  });
-  callback(oAuth2Client);
-  // }
-  // console.log("Authorize this app by visiting this url:", authUrl);
-  // const rl = readline.createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  // });
-  // rl.question("Enter the code from that page here: ", (code) => {
-  //   rl.close();
-  //   oAuth2Client.getToken(code, (err, token) => {
-  //     if (err) return console.error("Error while trying to retrieve access token", err);
-  //     oAuth2Client.setCredentials(token);
-  //     // Store the token to disk for later program executions
-  //     fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-  //       if (err) return console.error(err);
-  //       console.log("Token stored to", TOKEN_PATH);
-  //     });
-  //     callback(oAuth2Client);
-  //   });
-  // });
-}
+// function getNewToken(oAuth2Client, callback) {
+//   // If modifying these scopes, delete token.json.
+//   // const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+//   // // The file token.json stores the user's access and refresh tokens, and is
+//   // // created automatically when the authorization flow completes for the first
+//   // // time.
+//   // const readline = require("readline");
+//   // const authUrl = oAuth2Client.generateAuthUrl({
+//   //   access_type: "offline",
+//   //   scope: SCOPES,
+//   // });
+//   // if (process.env.GOOGLE_TOKEN) {
+//   oAuth2Client.setCredentials(process.env.GOOGLE_TOKEN);
+//   fs.writeFile(TOKEN_PATH, JSON.stringify(process.env.GOOGLE_TOKEN), (err) => {
+//     if (err) return console.error(err);
+//     console.log("Token stored to", TOKEN_PATH);
+//   });
+//   callback(oAuth2Client);
+//   // }
+//   // console.log("Authorize this app by visiting this url:", authUrl);
+//   // const rl = readline.createInterface({
+//   //   input: process.stdin,
+//   //   output: process.stdout,
+//   // });
+//   // rl.question("Enter the code from that page here: ", (code) => {
+//   //   rl.close();
+//   //   oAuth2Client.getToken(code, (err, token) => {
+//   //     if (err) return console.error("Error while trying to retrieve access token", err);
+//   //     oAuth2Client.setCredentials(token);
+//   //     // Store the token to disk for later program executions
+//   //     fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+//   //       if (err) return console.error(err);
+//   //       console.log("Token stored to", TOKEN_PATH);
+//   //     });
+//   //     callback(oAuth2Client);
+//   //   });
+//   // });
+// }
