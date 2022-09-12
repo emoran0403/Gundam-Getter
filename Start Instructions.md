c8d4ecfee86e140992f86eea5ab0818a04af4df6
Eric Moran <emoran0403@gmail.com>
2022-09-12 17:59:33 -0400

    updating start script, testing deployability

--- Start Instructions.md
+++ Start Instructions.md
@@ -1,4 +1,4 @@
-###Follow these steps to install everything and launch Gundam Getter
+Follow these steps to install everything and launch Gundam Getter
 
 1. Install Node by clicking this link:
    https://nodejs.org/dist/v16.17.0/node-v16.17.0-x64.msi
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,12 +5,6 @@ export const GOOGLEINFO = {
   spreadSheetID: process.env.GOOGLE_SPREADSHEET_ID,
   sheetID: process.env.GOOGLE_SHEET_ID,
   googleApiKey: process.env.GOOGLE_API_KEY,
-  GOOGLE_TOKEN: process.env.GOOGLE_TOKEN,
-};
-
-export const CLIENTINFO = {
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
 };
 
 /**
